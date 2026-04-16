231ff3047f819438a0b717d52f3f1f7e6ff87369
Your Name AMIR ANSARI <your-email@example.com>
2026-04-16 17:36:04 +0530

    nothing

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,8 +12,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6 hover:bg-gray-100 rounded hover:text-black transition-all duration-300 text-gray-600 cursor-pointer px-1 py-1'>
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className=' text-gray-500'>226028 Indra Canal Fiazabad Road <br /> Apna Room, Lucknow, India </p>
-          <p className=' text-gray-500'>Tel: +91-757-195-3107<br /> Email:peetamber@gmail.com</p>
+          <p className=' text-gray-500'>226028 Indra Canal Fiazabad Road <br />  Lucknow, India </p>
+          <p className=' text-gray-500'>Tel: +91-757-195-3107, <br /> Email:appointix@gmail.com</p>
           <p className=' font-semibold text-lg text-gray-600'>CAREERS AT Appointix</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
