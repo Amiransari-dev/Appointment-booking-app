d34930a7002f0db75c75b816907c8714198bd1b4
Amir Ansari <amiransari06dev@gmail.com>
2026-04-16 23:58:53 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,8 +7,9 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-
+    const backendUrl "https://appointment-booking-app-backend-fc9a.onrender.com"
+// import.meta.env.VITE_BACKEND_URL
+    
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
     const [userData, setUserData] = useState(false)
